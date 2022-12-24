45ccf6cb722cb6f47bbd0a88670f0d5671f197d4
TJ <tj@TJs-MacBook-Pro.local>
2022-12-24 10:29:51 -0500

    NY App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{lineHeight: 0.25, padding: 0.5 }}>
+        <img src={logo} alt="logo" />
+        </div>
+        <p>NY SYSTEMS Inc.</p>
+        <div style={{ color: 'blue', lineHeight : 1.25, padding: 20, alignItems: 'right', justifyContent: 'left'}}>
+        <button>REGISTER</button>
+        <button>LOGIN</button>
+        </div>
       </header>
-    </div>
+</div>
   );
 }
 
